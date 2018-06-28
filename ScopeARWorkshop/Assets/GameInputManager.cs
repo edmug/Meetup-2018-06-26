ee3bca7d869919333e8418106550cdb9ed1e2a8d
Chris C <chris.cumming@saturdaymp.com>
2018-06-27 19:33:36 -0600

    Added the Scope AR Workshop files.
    Copied from: https://bitbucket.org/scopear/scopehololenstutorial.git

---
+++ ScopeARWorkshop/Assets/GameInputManager.cs
@@ -0,0 +1,72 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+interface IInputSource
+{
+    event Action<Ray> OnTap;
+}
+
+// When running a holographic project in the Unity editor, tap events are not triggered unless
+// you have an xbox controller. This is a little workaround to use the "enter" key to trigger a
+// tap event.
+class EditorKeyPressSource : MonoBehaviour, IInputSource
+{
+    const KeyCode EditorKeyCode = KeyCode.Return;
+
+    public event Action<Ray> OnTap;
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(EditorKeyCode))
+        {
+            OnTap(GetCameraRayToCenterOfScreen());
+        }
+#endif
+    }
+
+    private Ray GetCameraRayToCenterOfScreen()
+    {
+        var screenCenterPoint = new Vector3(Screen.width / 2, Screen.height / 2);
+        return Camera.main.ScreenPointToRay(screenCenterPoint);
+    }
+}
+
+// Input source powered by the Mixed Reality Toolkit's GestureRecognizer.
+class GestureRecognizerSource : MonoBehaviour, IInputSource
+{
+    public event Action<Ray> OnTap;
+
+    private void Start()
+    {
+        // TODO: Set up the Mixed Reality Toolkit's GestureRecognizer to raise Tap events
+    }
+}
+
+public class GameInputManager : MonoBehaviour
+{
+    Type[] inputSourceTypes = new Type[]
+    {
+        typeof(EditorKeyPressSource),
+        typeof(GestureRecognizerSource)
+    };
+
+    [SerializeField]
+    private GameObject ProjectilePrefab;
+
+    private void Start()
+    {
+        foreach (var sourceType in inputSourceTypes)
+        {
+            IInputSource inputSource = (IInputSource)gameObject.AddComponent(sourceType);
+            inputSource.OnTap += InputSource_OnTap;
+        }
+    }
+
+    private void InputSource_OnTap(Ray ray)
+    {
+        // TODO: Launch a basketball!
+    }
+}
---
+++ ScopeARWorkshop/Assets/VoiceControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceControl : MonoBehaviour
+{
+    private KeywordRecognizer recognizer;
+
+    private Dictionary<string, Action> keywords;
+
+    void Start ()
+    {
+        // TODO: Setup a KeywordRecognizer that let's us use voice commands to place the hoop
+    }
+}
